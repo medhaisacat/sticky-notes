e82353bc47afb1c76c867cb8bcee94437095a03f
Medha S N <meedddhhhhaaaaa@gmail.com>
2022-06-16 13:05:39 +0530

    Add preview

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,17 @@
 var form = document.querySelector('form');
 const list = document.querySelector('ol');
+const text = document.querySelector('#textBar');
+var preview = document.querySelector('#preview');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     appendToDisplay(form.elements.todoItem.value);
     form.elements.todoItem.value = '';
+    preview.innerText = '';
+})
+
+text.addEventListener('input', () => {
+    preview.innerText = text.value;
 })
 
 function appendToDisplay(text) {
@@ -12,6 +19,10 @@ function appendToDisplay(text) {
     let checkbox = document.createElement('input');
     checkbox.type = "checkbox";
 
+    if(text === '') {
+        alert('Empty note!');
+        return;
+    }
     note.innerText = text;
     note.appendChild(checkbox);
     note.classList = "inputBar"
